35bf83ae291a2dda2365672d8e3313f4abb2570d
SirWingedMan004 <160008979+SirWingedMan004@users.noreply.github.com>
2024-03-01 20:53:19 +1300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('rsvpButton').addEventListener('click', function() {
-    alert('Thank you for your interest! Please send your RSVP to communitygreeninitiative@example.com');
-});
